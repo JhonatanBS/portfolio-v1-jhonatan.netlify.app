0becb284f4ddcb55c5d091a48660a2c4d4899a39
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-01-25 10:50:23 -0300

    Adição de ícone do botão de abrir e fechar usando javaScript.

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,18 @@
 const botao = document.querySelector(".botao-menu");
+const iconeBotao = document.querySelector(".bi-list");
 
 function hamburger(){
     const menu = document.querySelector(".menu");
     menu.classList.toggle("ative");
+    
+    if(iconeBotao.classList.contains("bi-list")){
+        iconeBotao.classList.remove("bi-list");
+        iconeBotao.classList.add("bi-x-lg");
+    }
+    else if(iconeBotao.classList.contains("bi-x-lg")){
+        iconeBotao.classList.remove("bi-x-lg");
+        iconeBotao.classList.add("bi-list");
+    }
 }
 
 botao.addEventListener("click", hamburger);
